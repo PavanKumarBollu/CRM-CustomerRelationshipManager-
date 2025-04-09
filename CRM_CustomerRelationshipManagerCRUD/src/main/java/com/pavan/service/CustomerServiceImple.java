d3586e054dd3ce705aebb9665abc08695e8ab2f1
PavanKumarB <bollupavankumar12@gmail.com>
2025-04-10 04:36:26 +0530

    CRM Basic Project Setup For Getting all the employes for the Db

--- CRM_CustomerRelationshipManagerCRUD/src/main/java/com/pavan/service/CustomerServiceImple.java
+++ CRM_CustomerRelationshipManagerCRUD/src/main/java/com/pavan/service/CustomerServiceImple.java
@@ -3,10 +3,12 @@ package com.pavan.service;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import com.pavan.dao.ICustomerDao;
 import com.pavan.model.Customer;
 
+@Service
 public class CustomerServiceImple implements ICustomerService {
 
 	@Autowired
@@ -15,7 +17,7 @@ public class CustomerServiceImple implements ICustomerService {
 	@Override
 	public List<Customer> getAllCustomer() {
 		
-		return null;
+		return (List<Customer>) dao.findAll();
 	}
 
 }
