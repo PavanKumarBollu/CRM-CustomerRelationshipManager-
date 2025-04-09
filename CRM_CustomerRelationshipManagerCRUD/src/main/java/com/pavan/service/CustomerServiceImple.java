7bdd45b7f01026d84bbf99757757a6e5ffa6a21d
PavanKumarB <bollupavankumar12@gmail.com>
2025-04-10 04:25:43 +0530

    CRM Project Creation

---
+++ CRM_CustomerRelationshipManagerCRUD/src/main/java/com/pavan/service/CustomerServiceImple.java
@@ -0,0 +1,15 @@
+package com.pavan.service;
+
+import java.util.List;
+
+import com.pavan.model.Customer;
+
+public class CustomerServiceImple implements ICustomerService {
+
+	@Override
+	public List<Customer> getAllCustomer() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
