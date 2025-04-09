9c85b47e00a07314d5ff518b6509a97e9794feb6
PavanKumarB <bollupavankumar12@gmail.com>
2025-04-10 03:38:00 +0530

    CRM Project Creation

---
+++ CRM_CustomerRelationshipManagerCRUD/src/main/java/com/pavan/CrmCustomerRelationshipManagerCrudApplication.java
@@ -0,0 +1,13 @@
+package com.pavan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrmCustomerRelationshipManagerCrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrmCustomerRelationshipManagerCrudApplication.class, args);
+	}
+
+}
