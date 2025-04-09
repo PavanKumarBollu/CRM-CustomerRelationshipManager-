dbbf53307b0973e39ff8ed949367923a0fcdd792
PavanKumarB <bollupavankumar12@gmail.com>
2025-04-10 04:45:04 +0530

    CRM Basic Project Setup For Getting all the employes for the Db

--- CRM_CustomerRelationshipManagerCRUD/src/main/java/com/pavan/controller/CustomerController.java
+++ CRM_CustomerRelationshipManagerCRUD/src/main/java/com/pavan/controller/CustomerController.java
@@ -1,5 +1,28 @@
 package com.pavan.controller;
 
-public class CustomerController {
+import java.util.List;
+import java.util.Map;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.pavan.model.Customer;
+import com.pavan.service.ICustomerService;
+
+@Controller
+@RequestMapping("/customer")
+public class CustomerController {
+	
+	@Autowired
+	private ICustomerService service;
+	
+	@GetMapping(value="/list")
+	public String listCustomers(Map<String,Object> model)
+	{
+		List<Customer> allCustomer = service.getAllCustomer();
+		model.put("allCustomer", allCustomer);
+		return "list-customers";
+	}
 }
--- CRM_CustomerRelationshipManagerCRUD/src/main/webapps/WEB-INF/pages/list-customers.jsp
+++ CRM_CustomerRelationshipManagerCRUD/src/main/webapps/WEB-INF/pages/list-customers.jsp
@@ -7,6 +7,6 @@
 <title>Insert title here</title>
 </head>
 <body>
-
+<h1>Page loading soon....</h1>
 </body>
 </html>
