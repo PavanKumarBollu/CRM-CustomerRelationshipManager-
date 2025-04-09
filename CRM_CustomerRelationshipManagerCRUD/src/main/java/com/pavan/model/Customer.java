bdac06f6e5577909afd6c24d67a3fe6a377b5b81
PavanKumarB <bollupavankumar12@gmail.com>
2025-04-10 04:33:06 +0530

    CRM Basic Project Setup For Getting all the employes for the Db

--- CRM_CustomerRelationshipManagerCRUD/src/main/java/com/pavan/model/Customer.java
+++ CRM_CustomerRelationshipManagerCRUD/src/main/java/com/pavan/model/Customer.java
@@ -1,10 +1,23 @@
 package com.pavan.model;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Table(name = "customer")
+@Data
+@NoArgsConstructor
 public class Customer {
-
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+	private String firstName;
+	private String lastName;
+	private String email;
 }
