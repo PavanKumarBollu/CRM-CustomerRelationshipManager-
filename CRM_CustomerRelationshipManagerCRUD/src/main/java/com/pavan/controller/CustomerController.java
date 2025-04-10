6d833d7d70ef50bd5346fbb67ab27bd01e43a957
PavanKumarB <bollupavankumar12@gmail.com>
2025-04-10 18:49:45 +0530

    Working with the Insert Feature

--- CRM_CustomerRelationshipManagerCRUD/src/main/java/com/pavan/controller/CustomerController.java
+++ CRM_CustomerRelationshipManagerCRUD/src/main/java/com/pavan/controller/CustomerController.java
@@ -6,6 +6,7 @@ import java.util.Map;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -47,6 +48,11 @@ public class CustomerController {
 		return "customer-form";
 	}
 	
+	public String saveCustomer(@ModelAttribute Customer customer)
+	{
+		System.out.println(customer);
+		return "redirect:/customer/list";
+	}
 	
 	
 }
