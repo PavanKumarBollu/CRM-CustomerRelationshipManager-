3c5b3c265a12aa2631f5a56177c038f3716a0453
PavanKumarB <bollupavankumar12@gmail.com>
2025-04-10 18:53:49 +0530

    Working with the Insert Feature sucessfully collecting the values form the user and need to push the values to db

--- CRM_CustomerRelationshipManagerCRUD/src/main/java/com/pavan/controller/CustomerController.java
+++ CRM_CustomerRelationshipManagerCRUD/src/main/java/com/pavan/controller/CustomerController.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -47,7 +48,7 @@ public class CustomerController {
 		model.put("customer", new Customer());
 		return "customer-form";
 	}
-	
+	@PostMapping(value="/saveCustomer")
 	public String saveCustomer(@ModelAttribute Customer customer)
 	{
 		System.out.println(customer);
--- CRM_CustomerRelationshipManagerCRUD/src/main/webapp/WEB-INF/pages/list-customers.jsp
+++ CRM_CustomerRelationshipManagerCRUD/src/main/webapp/WEB-INF/pages/list-customers.jsp
@@ -8,7 +8,7 @@
 <head>
 <meta charset="UTF-8">
 <title>Insert title here</title>
-<link rel="stylesheet" type="text/css" href="css/style.css" />
+<link rel="stylesheet" type="text/css" href="${pageContext.request.contextPath}/css/style.css" />
 </head>
 <body>
 	<div id="wrapper">
